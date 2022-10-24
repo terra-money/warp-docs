47c05b1ba4f038c3021e02a5738678efdcafe1d5
Evan <87997759+evanorti@users.noreply.github.com>
2022-10-24 10:21:21 -0700

    Initial commit

---
+++ src/theme/Link/link-data/hyperlinks.js
@@ -0,0 +1,20 @@
+const hyperlinks = [
+  {
+    href: 'https://station.terra.money/',
+    innerHTML: 'Terra Station',
+  },
+  {
+    href: 'https://status.terra.money/',
+    innerHTML: 'Terra Status',
+  },
+  {
+    href: 'https://finder.terra.money/',
+    innerHTML: 'Terra Finder',
+  },
+  {
+    href: 'https://agora.terra.money/',
+    innerHTML: 'Terra Agora',
+  },
+]
+
+export default hyperlinks;
