cf1dabea57f56924a2d78b791a76b57e5fff36c1
evanorti <87997759+evanorti@users.noreply.github.com>
2022-12-16 14:34:49 -0800

    Update home-cards.js

--- src/components/Cards/card-data/home-cards.js
+++ src/components/Cards/card-data/home-cards.js
@@ -1,41 +1,41 @@
 const homeCards = [
   {
-    href: '/how/overview.html',
+    href: '/how/overview',
     //lightSvg: '/img/icons/buildanapp02.svg',
     darkSvg: '/img/cards/question-circle.svg',
     header: 'What is Warp?',
     summary: 'Learn about jobs, conditions, and keepers.'
   },
   {
-    href: '/get-started.html',
+    href: '/get-started',
     //lightSvg: '/img/icons/station/station.svg',
     darkSvg: '/img/cards/started.svg',
     header: 'Get started',
     summary: 'Create a job and start using Warp.'
   },
   {
-    href: '/example.html',
+    href: '/guides/example',
     //lightSvg: '/img/icons/buildanapp01.svg',
     darkSvg: '/img/cards/gear.svg',
     header: 'Job example',
     summary: 'Use a hands-on example to create a job.'
   },
   {
-    href: '/how/condition.html',
+    href: '/how/condition',
     //lightSvg: '/img/icons/mintnft.svg',
     darkSvg: '/img/cards/pencil-square.svg',
     header: 'Conditions',
     summary: 'Learn how to write conditions.'
   },
   {
-    href: '/how/query.html',
+    href: '/how/query',
     //lightSvg: '/img/icons/terra.svg',
     darkSvg: '/img/cards/chat-left.svg',
     header: 'Queries',
     summary: 'Learn about query expressions.'
   },
   {
-    href: '/how/state.html',
+    href: '/how/state',
     //lightSvg: '/img/icons/swap-1.svg',
     darkSvg: '/img/cards/diagram-2-fill.svg',
     header: 'Job state',
