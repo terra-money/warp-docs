9b072411e790e9e9d56f93f2c8094459e0ec0321
evanorti <87997759+evanorti@users.noreply.github.com>
2023-02-01 11:57:22 -0800

    Add preview image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ module.exports = async function config() {
     themeConfig:
       /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
-        image: 'img/Twitter Profile.png',
+        image: 'img/twitter-profile.png',
         docs: {
           sidebar: {
             hideable: false,
