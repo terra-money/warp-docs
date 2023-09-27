e605f4810595193eb191204583bfe7e3f58d7873
evanorti <ortiz@terra.money>
2023-09-27 16:16:56 -0400

    Update

---
+++ src/components/Cards/card-data/get-started-cards.js
@@ -0,0 +1,18 @@
+const getStartedCards = [
+  {  
+    href: '/guides/ui-tutorial',
+    //lightSvg: '/img/icons/sdk.svg',
+    darkSvg: '/img/icons/sdk.svg',
+    header: 'Warp UI tutorial',
+    summary: 'Learn how to create a job using the Warp App.'
+  },
+  {
+    href: '/guides/sdk-tutorial',
+    //lightSvg: '/img/icons/browser.svg',
+    darkSvg: '/img/icons/browser.svg',
+    header: 'Warp SDK tutorial',
+    summary: 'Learn how to use the Warp SDK. '
+  },
+]
+
+export default getStartedCards;
